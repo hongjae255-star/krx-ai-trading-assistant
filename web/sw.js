5dd21a22b6ec2363a2caeb31342cabbc5becdfc3
hongjae255-star <hongjae255@gmail.com>
2026-09-15 00:03:57 +0900

    Move PWA hosting to GitHub Pages

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'krx-ai-v7-static';
+const CACHE = 'krx-ai-v7.3-static';
 const ASSETS = ['./index.html','./styles.css','./runtime-config.js','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
